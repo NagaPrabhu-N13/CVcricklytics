03ff630d90d7dfbee04a95a267b112662798aa8f
Sopitha-77 <sopithasopitha7@gmail.com>
2025-08-14 19:50:03 +0530

    testing

--- src/components/kumar/flowchart.jsx
+++ src/components/kumar/flowchart.jsx
@@ -9,7 +9,7 @@ const generateUUID = () => {
     return v.toString(16);
   });
 };
-
+//hello
 const TournamentSplitter = {
   getOptimalGroups(teamCount) {
     if (teamCount === 4) return { groupCount: 1, size: 4 };
